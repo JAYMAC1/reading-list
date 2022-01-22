fe31413b7075de3feec83259ed5d09f31aa17b1b
James McNeil <mcneil_james@hotmail.com>
2022-01-22 09:32:36 +0000

    removed App css class from div

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,13 @@ export default function Home() {
     { title: 'the name of the wind', id: 1 },
     { title: 'the dragon reborn', id: 2 },
     { title: 'the final empire', id: 3 },
-    { title: 'the way of kings', id: 4 }
+    { title: 'the way of kings', id: 4 },
   ])
 
   return (
-    <div className="App">
+    <div>
       {books && <BookList books={books} />}
       <BookForm />
     </div>
-  );
+  )
 }
