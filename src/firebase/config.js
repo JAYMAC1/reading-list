1d2159b33b5cf54f46a8df3882cc4120030fbe75
James McNeil <mcneil_james@hotmail.com>
2022-01-22 09:43:57 +0000

    created firebase folder and initial fb config settings from the google app

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAs00-PTGVnYmMGT2bx03xM7fRLsbBNcx8',
+  authDomain: 'readinglistapp-c6162.firebaseapp.com',
+  projectId: 'readinglistapp-c6162',
+  storageBucket: 'readinglistapp-c6162.appspot.com',
+  messagingSenderId: '489686519443',
+  appId: '1:489686519443:web:5c48086080735ccf2e8766',
+}
