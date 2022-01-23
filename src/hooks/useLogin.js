305279f046b06ca02fae36e368b602599b65830b
James McNeil <mcneil_james@hotmail.com>
2022-01-23 16:02:19 +0000

    imported useAuthContext into custom hooks to use dispatch actions for users global state

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,15 +1,21 @@
 import { useState } from 'react'
+
+// context imports
+import { useAuthContext } from '../hooks/useAuthContext'
+
+// firebase imports
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../firebase/config'
 
 export const useLogin = () => {
   const [error, setError] = useState(null)
+  const { dispatch } = useAuthContext()
 
   const login = (email, password) => {
     setError(null)
     signInWithEmailAndPassword(auth, email, password)
       .then((res) => {
-        console.log('user is logged in: ', res.user)
+        dispatch({ type: 'LOGIN', payload: res.user })
       })
       .catch((err) => {
         setError(err.message)
