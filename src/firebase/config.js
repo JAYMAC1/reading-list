5dc5c72c25ed6866ab56cdb1ee84a17ca86a37a4
James McNeil <mcneil_james@hotmail.com>
2022-01-23 10:18:07 +0000

    imported firebase auth and imported as 'auth'

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAs00-PTGVnYmMGT2bx03xM7fRLsbBNcx8',
@@ -16,4 +17,7 @@ initializeApp(firebaseConfig)
 // init firestore database
 const db = getFirestore()
 
-export { db }
+// init firebase auth
+const auth = getAuth()
+
+export { db, auth }
