06be37a181fb2ebb64970dd7fd4fb441a1b5d04e
James McNeil <mcneil_james@hotmail.com>
2022-01-23 10:12:27 +0000

    import doc and deleteDoc to delete doc

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,21 @@
-export default function BookList({ books }) {
+import { db } from '../firebase/config'
+import { collection, doc, deleteDoc } from 'firebase/firestore'
 
+export default function BookList({ books }) {
   const handleClick = async (id) => {
+    const docRef = doc(db, 'books', id)
+    // await deleteDoc(doc(db,'books, id))
+    await deleteDoc(docRef)
     console.log(id)
   }
 
   return (
-    <div className="book-list">
+    <div className='book-list'>
       <ul>
-        {books.map(book => (
-          <li key={book.id} onClick={() => handleClick(book.id)}>{book.title}</li>
+        {books.map((book) => (
+          <li key={book.id} onClick={() => handleClick(book.id)}>
+            {book.title}
+          </li>
         ))}
       </ul>
     </div>
